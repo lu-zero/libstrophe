ff323c6a611c39e8000caf999f97801c472673a4
Matt Ingenthron <ingenthr@cep.net>
2009-09-30 17:48:35 -0700

    Added workaround for MacOS libtool.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 mkdir -p m4
-libtoolize
+UNAME=`uname -s`
+if [ $UNAME = "Darwin" ]; then
+glibtoolize --automake
+else
+libtoolize --automake
+fi
 aclocal
 automake --add-missing --foreign --copy
 autoconf
