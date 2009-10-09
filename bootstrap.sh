ecf5edc75dd8c1cbd5ac8ee990d48717cf2d9a10
Matt Ingenthron <ingenthr@cep.net>
2009-10-09 09:16:50 -0700

    Add the which dance for aclocal and the multitude of versions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+
+echo "libstrophe autogen.sh..."
 mkdir -p m4
 UNAME=`uname -s`
 if [ $UNAME = "Darwin" ]; then
@@ -7,6 +10,7 @@ glibtoolize --automake
 else
 libtoolize --automake
 fi
-aclocal
+ACLOCAL=`which aclocal-1.11 || which aclocal-1.10 || which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15`
+${ACLOCAL:-aclocal} $ACLOCALFLAGS || exit 1
 automake --add-missing --foreign --copy
 autoconf
